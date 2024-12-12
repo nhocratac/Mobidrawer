352dca5680c237d9e9f0ac6852f8f3eed649aea6
nhocratac <22521339@gm.uit.edu.vn>
2024-12-12 11:59:57 +0700

    deploy

--- client/assets/HelpCenterImages/index.ts
+++ client/assets/HelpCenterImages/index.ts
@@ -1,9 +1,9 @@
-import help1 from "../../assets/HelpCenterImages/help1.png"
-import help2 from "../../assets/HelpCenterImages/help2.png"
-import help3 from "../../assets/HelpCenterImages/help3.png"
-import help4 from "../../assets/HelpCenterImages/help4.png"
-import help5 from "../../assets/HelpCenterImages/help5.png"
-import help6 from "../../assets/HelpCenterImages/help6.png"
+import help1 from "@/assets/HelpCenterImages/help1.png"
+import help2 from "@/assets/HelpCenterImages/help2.png"
+import help3 from "@/assets/HelpCenterImages/help3.png"
+import help4 from "@/assets/HelpCenterImages/help4.png"
+import help5 from "@/assets/HelpCenterImages/help5.png"
+import help6 from "@/assets/HelpCenterImages/help6.png"
 
 const HelpCenterImages = [
     {
